4a9706db7d559503470f32d3623178844a1aaeb7
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-05-18 23:54:19 -0500

    Calculating room state correctly

--- src/room/room_api.rs
+++ src/room/room_api.rs
@@ -1,13 +1,27 @@
 use screeps::Room;
 
-use crate::memory::room_memory::{RoomMemory, RoomState};
+use crate::{
+    creep::roles::roles_api::Roles,
+    memory::room_memory::{RoomMemory, RoomState},
+    spawn::spawn_utils::get_living_creep_counts,
+};
 
-pub fn set_room_state(room: Room) {
-    let mut room_memory = RoomMemory::get(&room);
+pub fn set_room_state(room: &Room) {
+    let mut room_memory = RoomMemory::get(room);
 
-    // calculate room state here
-    let room_state = RoomState::BASIC;
+    let room_state = calculate_room_state(room);
 
     room_memory.room_state = room_state;
-    room_memory.set(&room);
+    room_memory.set(room);
+}
+
+fn calculate_room_state(room: &Room) -> RoomState {
+    let creep_counts = get_living_creep_counts(room);
+    let miner_count = creep_counts.get(&Roles::Miner).unwrap_or(&0);
+
+    if *miner_count < 1 {
+        return RoomState::BOOTSTRAP;
+    }
+
+    RoomState::BASIC
 }
